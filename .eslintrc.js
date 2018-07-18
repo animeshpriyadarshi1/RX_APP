0bcaa2c9bc16a1d329e49af9198a4f3885ed0518
Animesh Priyadarshi <apriyadarshi5@gmail.com>
2018-07-18 22:38:03 +0530

    Added ESlint as per google standard

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    env: {
+        es6: true,
+        node: true
+    },
+    extends: ["eslint:recommended", "google"],
+    parserOptions: {
+        ecmaFeatures: {
+            experimentalObjectRestSpread: true,
+            jsx: true
+        },
+        sourceType: "module"
+    },
+    plugins: ["react"],
+    rules: {
+        "react/jsx-uses-react": "error",
+        "react/jsx-uses-vars": "error",
+        indent: [2, 2],
+        "object-curly-spacing": ["error", "never"],
+        "no-console": "error",
+        "semi": "error",
+        "no-undefined": "error",
+        "arrow-parens": ["error", "always"],
+        "comma-dangle": ["error", "never"],
+        "no-underscore-dangle": ["error", {
+            allow: ["foo_", "_bar"]
+        }],
+        "max-len": ["error", {
+            code: 80
+        }],
+        "require-jsdoc": [
+            "error",
+            {
+                require: {
+                    FunctionDeclaration: true,
+                    MethodDefinition: false,
+                    ClassDeclaration: false,
+                    ArrowFunctionExpression: false,
+                    FunctionExpression: false
+                }
+            }
+        ]
+    }
+};
